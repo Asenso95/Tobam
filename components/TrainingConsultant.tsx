e66aaafafef49a7613b5e3a79107109aa19824ad
Ben Asenso <asenso95@gmail.com>
2026-09-08 13:03:07 +0000

    Update TrainingConsultant.tsx

--- components/TrainingConsultant.tsx
+++ components/TrainingConsultant.tsx
@@ -50,7 +50,7 @@ export default function TrainingConsultant() {
           their career advancement.
         </p>
 
-        <div className="bg-[#571244] p-6 mt-8 rounded-[8px]">
+        <div className="bg-[#571244] p-6 mt-6 rounded-[8px]">
         <ul className="grid gap-6 sm:grid-cols-2">
           {cards.map((card, index) => (
             <li
