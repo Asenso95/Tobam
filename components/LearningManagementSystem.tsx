ee8bc4750856e106f1e4f3f3386f3d4d6c8297ff
Ben Asenso <asenso95@gmail.com>
2026-09-08 14:50:58 +0000

    Update LearningManagementSystem.tsx

--- components/LearningManagementSystem.tsx
+++ components/LearningManagementSystem.tsx
@@ -14,7 +14,7 @@ const courses = [
 
 export default function LearningManagementSystem() {
   return (
-    <section aria-labelledby="lms-heading" className="mt-8 bg-[#e9e2e7] py-6 px-1 md:py-12 md:px-8"
+    <section aria-labelledby="lms-heading" className="mt-6 md:mt-8 bg-[#e9e2e7] py-6 px-1 md:py-12 md:px-8"
     >
       <div className="mx-auto grid max-w-6xl items-center gap-2 px-4 md:gap-10 px-6 lg:grid-cols-[2fr_3fr] lg:gap-[80px] lg:px-8 max-w-7xl">
         
