46af354b180cdcef79e830fec51d5b1f9a140d3c
Ben Asenso <asenso95@gmail.com>
2026-09-08 10:29:44 +0000

    Update CapacityDevelopment.tsx

--- components/CapacityDevelopment.tsx
+++ components/CapacityDevelopment.tsx
@@ -11,7 +11,7 @@ const bullets = [
 
 export default function CapacityDevelopment() {
   return (
-    <section aria-labelledby="capacity-heading" className="py-4 md:py-20">
+    <section aria-labelledby="capacity-heading" className="py-8 md:py-20">
       <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 max-w-7xl">
         <h2 id="capacity-heading" className="sr-only">
           Capacity Development
